bb65ebe4dff9b4308bff168bf087cac8bf730c86
maritza1818 <mmaronpu@unsa.edu.pe>
2024-10-22 17:19:09 -0500

    I learned conversion types

--- src/Phyton_Fundaments/basic.py
+++ src/Phyton_Fundaments/basic.py
@@ -1,1 +1,34 @@
 print("Hello World!!!!")
+age = 19
+age = 18 + 2
+price = 23.44
+first_name = "Maritza"
+me_before = False
+print(age)
+#myfirstexercise
+patient = "John Smith"
+age = 20
+state = "New"
+print(patient," ",age," ",state)
+#funcion input se usa para leer un valor desde la ventana del terminal
+#input function is used to read a value from the terminal window
+name = input("What is your name? ")
+print("Hola " + name)
+#tipos de datos
+#date types
+birth_year = input("Enter your birth year: ")
+#FUNCTIONS
+#---int()---float()---bool()--str()---
+#La funcion int convierte en entero mi cadena de texto ingresaada
+#The  int function converts my entered text string to an integer
+age = 2024 - int(birth_year)
+print(age)
+#Second Exercise
+num1 = float(input("Enter the number of years your michirelationship :3 "))
+num2 = float(input("Enter the exact number of years your michirelationship :3 "))
+print("The number years is : " + str(num1 + num2))
+#num1 = 1
+#num2 = 1.162536
+#print(float(num1) + num2)
+#int_num = num1 + int(num2)
+#print(int_num)
